f3c7090b69be87eff89e293d4a50873b1916a089
SivaSundar17 <shivalalitha17@gmail.com>
2023-08-23 22:11:35 +0530

    removed all

--- frontend/src/main/java/com/example/dod/repo/WalmartProductCustom.java
+++
@@ -1,6 +0,0 @@
-package com.example.dod.repo;
-
-public interface WalmartProductCustom {
-
-	void createIFullTextndex();
-}
--- frontend/src/main/java/com/example/dod/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package com.example.dod.service;
-
-public interface EmailService {
-
-	public String sendEmail(String to, String subject, String text);
-}
